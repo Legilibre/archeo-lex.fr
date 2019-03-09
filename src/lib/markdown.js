ecd0029916528a9a309983b722ba0c38f174ea28
Seb35 <seb35@seb35.fr>
2019-03-09 18:22:35 +0100

    Fix bug in Markdown call, improve Markdown

--- src/lib/markdown.js
+++ src/lib/markdown.js
@@ -3,13 +3,62 @@ function get_summary(markdown) {
 
 }
 
+function balance_html(text) {
+
+	let ltext = text.split(/(<div(?: [a-zA-Z]+="[^"]*")*>|<\/div>)/), i, ic = 0;
+
+	for( i=0; i<ltext.length; i++ ) {
+		if( ltext[i].substring(0, 4) === '<div' ) {
+			ic += 1;
+		} else if( ltext[i].substring(0, 6) === '</div>' ) {
+			ic -= 1;
+		}
+		if( ic < 0 ) {
+			ltext[i] = Array(-ic).fill('<div>').join('') + ltext[i];
+			ic = 0;
+			console.log([ic,ltext[i]]);
+		}
+	}
+
+	text = ltext.join('');
+	if( ic > 0 ) {
+		text += Array(ic).fill('</div>').join('');
+	}
+
+	return text;
+}
+
 function markdown2html(markdown) {
 
 	let html = '\n' + markdown + '\n'
 
-	html = html.replace(/^(#+) (.*)$/gm, (match, p1, p2) => { return '<h2 class="h' + p1.length + '">' + p2 + '</h2>' });
+	// Typography
+	html = html.replace(/[  _]?;/g, ' ;');
+	html = html.replace(/[  _]?:/g, ' :');
+	html = html.replace(/'/g, '’');
 
-	return html;
+	// Cleaning
+	html = html.replace(/<div[^\/>]*\/>/g, '');
+
+	// Transform articles
+	html = html.replace(/^(?:#+) Article ([^\n]+)\n((?:(?!#)[^\n]*\n*)*)/gm, (match, p1, p2) => {
+		return '<div id="article_' + p1 + '" class="article"><h3>Article ' + p1 + '</h3>\n' + balance_html(p2) + '</div>\n\n';
+	});
+
+	// Transform headers
+	html = html.replace(/^(#+) (.*)$/gm, (match, p1, p2) => {
+		return '<h2 class="h' + p1.length + ' hmod3r' + ((p1.length-1)%3) + '" id="' + p2.replace(/[  ]/g, '_').replace(/’/g, "'") + '">' + p2 + '</h2>';
+	});
+
+	// Lists
+	html = html.replace(/^((?:- [^\n]+\n)+)/gm, '<ul>\n$1</ul>\n\n');
+	html = html.replace(/^- ([^\n]+)\n/gm, '<li>$1</li>\n');
+
+	// Cleaning
+	html = html.replace(/<\/p>\n+<p(?=[> ])/g, '</p>\n<p');
+	html = html.replace(/\n{3,}/g, '\n\n');
+
+	return html.trim();
 }
 
 const markdown = {
