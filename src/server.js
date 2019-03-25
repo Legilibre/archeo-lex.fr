d34f928488da0f7f0387ed8d0ccf9c32b1ab1f72
Seb35 <seb35@seb35.fr>
2019-03-25 19:43:25 +0100

    Add Content-Security-Policy

--- src/server.js
+++ src/server.js
@@ -1,15 +1,41 @@
 import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
+import csp from 'helmet-csp';
+import uuid from 'uuid/v4';
 import * as sapper from '../__sapper__/server.js';
 import config from './config.js';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
+let connectSrc = ["'self'"];
+if( dev ) {
+	connectSrc.push(config.devServer);
+}
+const base = '/' + config.base.replace(/\/+/g, '/').replace(/^\//, '').replace(/\/$/, '');
+
+polka()
+	.use((req, res, next) => {
+		res.locals = {}
+		res.locals.nonce = uuid()
+		next()
+	})
+	.use(csp({
+		directives: {
+			defaultSrc: ["'none'"],
+			styleSrc: ["'self'", "'unsafe-inline'"],
+			scriptSrc: ["'self'", "'unsafe-eval'", (req, res) => `'nonce-${res.locals.nonce}'`],
+			fontSrc: ["'self'"],
+			baseUri: ["'self'"],
+			connectSrc: connectSrc,
+			workerSrc: ["'self'"],
+			imgSrc: ["'self'"],
+		},
+		reportOnly: dev
+	}))
 	.use(
-		config.base === '' ? '/' : config.base,
+		base,
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
